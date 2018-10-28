927cde7051ac7408c57f11c219de1e288c3a81e0
sreedhar <sreedhar@classroom>
2018-10-28 10:10:35 +0530

    new folder

---
+++ GIT_Proj/src/PAc1/classA.java
@@ -0,0 +1,13 @@
+package PAc1;
+
+public class classA {
+	public void method1()
+	{
+		System.out.println("Method1");
+	}
+	
+	public void method2()
+	{
+		System.out.println("Method2");
+	}
+}
---
+++ GIT_Proj/src/PAc1/classB.java
@@ -0,0 +1,13 @@
+package PAc1;
+
+public class classB
+{
+	public void method1()
+	{
+		System.out.println("Method1");
+	}
+	public void method2()
+	{
+		System.out.println("Method2");
+	}
+}
